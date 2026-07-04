870a0aff6a96bc718474468f3affedf76992771d
Manoranjan <123184431+Manoranjan16@users.noreply.github.com>
2026-07-04 23:27:28 +0530

    added streamlit file

---
+++ app.py
@@ -0,0 +1,73 @@
+import joblib
+import streamlit as st
+import pandas as pd
+
+#Model Load
+model = joblib.load("delivery_time_model.pkl")
+encoded = joblib.load('data_encoding.pkl')
+features = joblib.load('fetures_name.pkl')
+scale = joblib.load('Scaled.pkl')
+
+st.set_page_config(page_title='Food Delivery Time Prediction')
+st.title('Food Delivery Time Preidction')
+st.markdown("Predict the estimated delivery time using a trained Machine Learning model.")
+st.sidebar.title("📌 About Project")
+st.sidebar.write("""
+This ML model predicts food delivery time based on:
+- Distance
+- Weather
+- Traffic
+- Time of Day
+- Vehicle Type
+- Preparation Time
+- Courier Experience
+""")
+
+# User Input
+col1, col2 = st.columns(2)
+with col1:
+    distance = st.number_input('Distanc (km)', min_value=0.0, value=5.0)
+    weather = st.selectbox('Weather', ['Windy', 'Clear', 'Foggy', 'Rainy', 'Snowy'])
+    traffic = st.selectbox("Traffic Level", ['Low', 'Medium', 'High'])
+    time = st.selectbox("Time of day", ['Afternoon', 'Evening', 'Night', 'Morning'])
+with col2:
+    vehicle = st.selectbox("Vehicle", ['Scooter', 'Bike', 'Car'])
+    preparation = st.number_input('Preparation Time (Minute)', min_value=1, value=15)
+    experience = st.number_input("Experience (year)", min_value=0.0, value=2.0)
+
+#Create DataFrame
+input_df = pd.DataFrame({
+    'Distance_km': [distance],
+    'Weather': [weather],
+    'Traffic_Level': [traffic],
+    'Time_of_Day': [time],
+    'Vehicle_Type': [vehicle],
+    'Preparation_Time_min': [preparation],
+    'Courier_Experience_yrs': [experience]
+})
+
+#Convert to encoding (dummy variable)
+input_df = pd.get_dummies(input_df)
+
+#add missing columns
+for col in features:
+    if col not in input_df.columns:
+        input_df[col] = 0
+
+#Arrange columns in training order
+input_df = input_df[features]
+
+#prediction
+if st.button("🚀 Predict Delivery Time", use_container_width=True):
+    prediction = model.predict(input_df)
+
+    st.success(f"Estimated Delivery Time : {prediction[0]:.2f} minutes")
+
+#Summary
+st.subheader("Input Summary")
+st.dataframe(input_df)
+
+# st.sidebar.subheader("Model Performance")
+# st.sidebar.metric("R² Score", "0.83")
+# st.sidebar.metric("MAE", "0.25")
+# st.sidebar.metric("RMSE", "0.38")
